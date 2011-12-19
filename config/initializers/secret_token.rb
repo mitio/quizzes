89280941ee830903cf5baa5fb96111b1ec191710
Dimitar Dimitrov <wireman@gmail.com>
2011-12-20 00:56:16 +0200

    Generate a default Rails 3.1.3 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quizzes::Application.config.secret_token = 'f3eee597cd68367c50a10d95b8dbe716393ef4307c8e78c60f37a0c7cf824985062f2175997fc3010c17a75b7c3ab8e0f7bbf9ddd2f482edde37f821ba725573'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Quizzes::Application.config.session_store :cookie_store, key: '_quizzes_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Quizzes::Application.config.session_store :active_record_store
