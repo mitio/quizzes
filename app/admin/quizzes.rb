e9b50839988497fe70482e8b7f67a3bd4f4f4766
Dimitar Dimitrov <wireman@gmail.com>
2011-12-21 01:18:39 +0200

    Add a link to the nested questions from the quizzes listing, in the admin

--- app/admin/quizzes.rb
+++ app/admin/quizzes.rb
@@ -4,6 +4,9 @@ ActiveAdmin.register Quiz do
     column :name
     column :created_at
     column :updated_at
+    column :questions do |quiz|
+      link_to quiz.questions.size, admin_quiz_questions_path(quiz)
+    end
     default_actions
   end
 end
