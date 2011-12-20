79851b04db0291de0c5cbc6e20ba4e4b4030af6d
Dimitar Dimitrov <wireman@gmail.com>
2011-12-21 00:53:04 +0200

    Remove unneeded respond_to calls in QuestionsController to clear things up

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -2,36 +2,18 @@ class QuestionsController < ApplicationController
   before_filter :load_quiz
 
   # GET /questions
-  # GET /questions.json
   def index
     @questions = @quiz.questions
-
-    respond_to do |format|
-      format.html # index.html.erb
-      format.json { render json: @questions }
-    end
   end
 
   # GET /questions/1
-  # GET /questions/1.json
   def show
     @question = @quiz.questions.find(params[:id])
-
-    respond_to do |format|
-      format.html # show.html.erb
-      format.json { render json: @question }
-    end
   end
 
   # GET /questions/new
-  # GET /questions/new.json
   def new
     @question = @quiz.questions.build
-
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @question }
-    end
   end
 
   # GET /questions/1/edit
@@ -40,47 +22,33 @@ class QuestionsController < ApplicationController
   end
 
   # POST /questions
-  # POST /questions.json
   def create
     @question = @quiz.questions.build(params[:question])
 
-    respond_to do |format|
-      if @question.save
-        format.html { redirect_to [@quiz, @question], notice: 'Question was successfully created.' }
-        format.json { render json: @question, status: :created, location: @question }
-      else
-        format.html { render action: "new" }
-        format.json { render json: @question.errors, status: :unprocessable_entity }
-      end
+    if @question.save
+      redirect_to [@quiz, @question], notice: 'Question was successfully created.'
+    else
+      render action: "new"
     end
   end
 
   # PUT /questions/1
-  # PUT /questions/1.json
   def update
     @question = @quiz.questions.find(params[:id])
 
-    respond_to do |format|
-      if @question.update_attributes(params[:question])
-        format.html { redirect_to [@quiz, @question], notice: 'Question was successfully updated.' }
-        format.json { head :ok }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @question.errors, status: :unprocessable_entity }
-      end
+    if @question.update_attributes(params[:question])
+      redirect_to [@quiz, @question], notice: 'Question was successfully updated.'
+    else
+      render action: "edit"
     end
   end
 
   # DELETE /questions/1
-  # DELETE /questions/1.json
   def destroy
     @question = @quiz.questions.find(params[:id])
     @question.destroy
 
-    respond_to do |format|
-      format.html { redirect_to quiz_questions_url(@quiz) }
-      format.json { head :ok }
-    end
+    redirect_to quiz_questions_url(@quiz)
   end
 
   private
