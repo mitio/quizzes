d207942b3a08dffc0093a94d9d4a56ac6f75b16a
Dimitar Dimitrov <me@ddimitrov.name>
2012-01-18 21:11:57 +0200

    Ensure the title of a quiz question is unique
    In the scope of the quiz, of course. Two different quizzes can still each have a question  titled "How old are you?".

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,5 +2,6 @@ class Question < ActiveRecord::Base
   belongs_to :quiz
 
   validates_presence_of :title
+  validates_uniqueness_of :title, scope: :quiz_id
   validates_numericality_of :position
 end
