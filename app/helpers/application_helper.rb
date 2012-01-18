14e933a20e19d429c91356158cfd8629d0809310
Dimitar Dimitrov <me@ddimitrov.name>
2012-01-18 21:40:16 +0200

    Extract the validation errors rendering in a helper
    ApplicationHelper#errors_on(instance) will make our code DRYer.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def errors_on(instance)
+    return unless instance.errors.any?
+
+    model = instance.class
+
+    error_box_header = content_tag :h2 do
+      t :header, count: instance.errors.count, model: model.model_name.human, scope: [model.i18n_scope, :errors, :template]
+    end
+
+    content_tag :div, id: 'error_explanation' do
+      error_box_header +
+      content_tag(:ul) do
+        instance.errors.full_messages.map { |msg| content_tag :li, msg }.join('').html_safe
+      end
+    end
+  end
 end
